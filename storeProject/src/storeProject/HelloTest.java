8a5e04818f196f1164ce30d7a4df5e739e156355
JH <ygosuygosu@outlook.com>
2018-08-06 19:21:00 +0900

    ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ

--- storeProject/src/storeProject/HelloTest.java
+++ storeProject/src/storeProject/HelloTest.java
@@ -5,12 +5,14 @@ public class HelloTest {
 		System.out.println("일꾼님들 안녕하세요");
 
 		System.out.println("홍달링");
-		System.out.println("그랜드카니발");
 		System.out.println("달링test2");
 
-		System.out.println("그랜드카니발9인승 20만원");
-		System.out.println("그랜드카니발9인승 20만원");
-		System.out.println("그랜드카니발9인승 20만원");
-		System.out.println("그랜드카니발9인승 20만원");
+		System.out.println("그랜드카니발 9인승 20만원");
+		System.out.println("그랜드카니발 9인승 20만원");
+		System.out.println("그랜드카니발 9인승 20만원");
+		System.out.println("그랜드카니발 12인승 20만원");
+		System.out.println("그랜드카니발 12인승 20만원");
+		
+		System.out.println("홍달림 이클립스 터져버림");
 	}
 }
