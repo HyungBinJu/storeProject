3784d38ab7db1e086ec6993a3168e0c057ecea22
JH <ygosuygosu@outlook.com>
2018-08-06 19:19:05 +0900

    카니발 렌터카 24시간

--- storeProject/src/storeProject/HelloTest.java
+++ storeProject/src/storeProject/HelloTest.java
@@ -8,37 +8,9 @@ public class HelloTest {
 		System.out.println("그랜드카니발");
 		System.out.println("달링test2");
 
-		System.out.println("달링test");
-
-
-		System.out.println("ㅋㅋㅋㅋㅋㅋ");
-
-		System.out.println("테스트7sdf");
-		System.out.println("테스트6sdf");
-		System.out.println("asdf테스트3sdf3");
-		System.out.println("adsf테스트sdf5");
-		System.out.println("asdf테스트sdf5");
-		System.out.println("테스트4");
-		System.out.println("테스트sdf2"); 
-		System.out.println("테스트");
-		
-		System.out.println("안녕");
-
-		System.out.println("스타렉스9인승");
-		System.out.println("그랜드카니발12인승");
-		System.out.println("그랜드카니발12인승");
-		System.out.println("그랜드카니발12인승");
-		System.out.println("그랜드카니발12인승");
-		System.out.println("그랜드카니발12인승");
-		System.out.println("스타렉스9인승");
-		System.out.println("스타렉스9인승");
-		System.out.println("스타렉스9인승");
-		System.out.println("스타렉스9인승");
-		System.out.println("스타렉스9인승");
-		System.out.println("안녕");
-		System.out.println("안녕");
-		System.out.println("안녕");
-		System.out.println("안녕");
-		
+		System.out.println("그랜드카니발9인승 20만원");
+		System.out.println("그랜드카니발9인승 20만원");
+		System.out.println("그랜드카니발9인승 20만원");
+		System.out.println("그랜드카니발9인승 20만원");
 	}
 }
